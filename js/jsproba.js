cc80065c4ef07fbc155423418859c32370cfd10a
Jelena Dokic <jelena.dpk@gmail.com>
2017-07-02 15:36:49 +0200

    all

---
+++ js/jsproba.js
@@ -0,0 +1,75 @@
+var images = {
+	"h1": ['images/h1/PH-NemanjaZivkovic 01.jpg','images/h1/PH-NemanjaZivkovic 02.jpg','images/h1/PH-NemanjaZivkovic 04.jpg','images/h1/PH-NemanjaZivkovic 16.jpg','images/h1/PH-NemanjaZivkovic 17.jpg','images/h1/PH-NemanjaZivkovic 21.jpg','images/h1/PH-NemanjaZivkovic 24.jpg'],
+	"h2": ['images/h2/PH-NemanjaZivkovic 05.jpg','images/h2/PH-NemanjaZivkovic 06.jpg','images/h2/PH-NemanjaZivkovic 07.jpg','images/h2/PH-NemanjaZivkovic 10.jpg','images/h2/PH-NemanjaZivkovic 11.jpg','images/h2/PH-NemanjaZivkovic 22.jpg','images/h2/PH-NemanjaZivkovic 27.jpg'],
+	"h3": ['images/h3/PH-NemanjaZivkovic 28.jpg','images/h3/PH-NemanjaZivkovic 32.jpg','images/h3/PH-NemanjaZivkovic 35.jpg','images/h3/PH-NemanjaZivkovic 48.jpg','images/h3/PH-NemanjaZivkovic 51.jpg','images/h3/PH-NemanjaZivkovic 52.jpg'],
+	"h4": ['images/h4/PH-NemanjaZivkovic 40.jpg','images/h4/PH-NemanjaZivkovic 36.jpg','images/h4/PH-NemanjaZivkovic 41.jpg','images/h4/PH-NemanjaZivkovic 42.jpg','images/h4/PH-NemanjaZivkovic 44.jpg'],
+	"h5": ['images/h5/PH-NemanjaZivkovic 54.jpg','images/h5/PH-NemanjaZivkovic 53.jpg','images/h5/PH-NemanjaZivkovic 59.jpg','images/h5/PH-NemanjaZivkovic 60.jpg','images/h5/PH-NemanjaZivkovic 61.jpg'],
+	"h6": ['images/h6/PH-NemanjaZivkovic 64.jpg','images/h6/PH-NemanjaZivkovic 68.jpg','images/h6/PH-NemanjaZivkovic 69.jpg','images/h6/PH-NemanjaZivkovic 70.jpg','images/h6/PH-NemanjaZivkovic 71.jpg'],
+	"h7": ['images/h7/PH-NemanjaZivkovic 73.jpg','images/h7/PH-NemanjaZivkovic 77.jpg','images/h7/PH-NemanjaZivkovic 78.jpg'],
+	"h8": ['images/h8/PH-NemanjaZivkovic 80.jpg','images/h8/PH-NemanjaZivkovic 82.jpg','images/h8/PH-NemanjaZivkovic 83.jpg','images/h8/PH-NemanjaZivkovic 84.jpg','images/h8/PH-NemanjaZivkovic 85.jpg'],
+	"h9": ['images/h9/PH-NemanjaZivkovic 94.jpg','images/h9/PH-NemanjaZivkovic 86.jpg','images/h9/PH-NemanjaZivkovic 87.jpg','images/h9/PH-NemanjaZivkovic 97.jpg'],
+	"h10": ['images/h10/PH-NemanjaZivkovic 99.jpg','images/h10/PH-NemanjaZivkovic 100.jpg','images/h10/PH-NemanjaZivkovic 101.jpg'],
+	"h11": ['images/h11/PH-NemanjaZivkovic 102.jpg','images/h11/PH-NemanjaZivkovic 104.jpg','images/h11/PH-NemanjaZivkovic 105.jpg','images/h11/PH-NemanjaZivkovic 106.jpg','images/h11/PH-NemanjaZivkovic 107.jpg'],
+	"h12": ['images/h12/PH-NemanjaZivkovic 141.jpg','images/h12/PH-NemanjaZivkovic 143.jpg'],
+	"h13": ['images/h13/PH-NemanjaZivkovic 145.jpg','images/h13/PH-NemanjaZivkovic 146.jpg','images/h13/PH-NemanjaZivkovic 147.jpg','images/h13/PH-NemanjaZivkovic 150.jpg'],
+	"h14": ['images/h14/PH-NemanjaZivkovic 153.jpg','images/h14/PH-NemanjaZivkovic 154.jpg','images/h14/PH-NemanjaZivkovic 148.jpg','images/h14/PH-NemanjaZivkovic 151.jpg'],
+	"h15": ['images/h15/PH-NemanjaZivkovic 158.jpg','images/h15/PH-NemanjaZivkovic 159.jpg'],
+	"h16": ['images/h16/PH-NemanjaZivkovic 166.jpg','images/h16/PH-NemanjaZivkovic 168.jpg','images/h16/PH-NemanjaZivkovic 169.jpg'],
+	"h17": ['images/h17/PH-NemanjaZivkovic 201.jpg','images/h17/PH-NemanjaZivkovic 202.jpg','images/h17/PH-NemanjaZivkovic 203.jpg'],
+	"bip": ['images/bip/PH-NemanjaZivkovic 110.jpg','images/bip/PH-NemanjaZivkovic 111.jpg','images/bip/PH-NemanjaZivkovic 112.jpg','images/bip/PH-NemanjaZivkovic 113.jpg'],
+	"bis1": ['images/bis1/PH-NemanjaZivkovic 119.jpg','images/bis1/PH-NemanjaZivkovic 120.jpg','images/bis1/PH-NemanjaZivkovic 121.jpg'],
+	"bis2": ['images/bis2/PH-NemanjaZivkovic 124.jpg','images/bis2/PH-NemanjaZivkovic 129.jpg','images/bis2/PH-NemanjaZivkovic 130.jpg'],
+	"bis3": ['images/bis3/PH-NemanjaZivkovic 136.jpg','images/bis3/PH-NemanjaZivkovic 139.jpg','images/bis3/PH-NemanjaZivkovic 140.jpg'],
+	"bis4": ['images/bis4/PH-NemanjaZivkovic 163.jpg','images/bis4/PH-NemanjaZivkovic 164.jpg','images/bis4/PH-NemanjaZivkovic 165.jpg'],
+	"bish1": ['images/bish1/PH-NemanjaZivkovic 171.jpg','images/bish1/PH-NemanjaZivkovic 172.jpg','images/bish1/PH-NemanjaZivkovic 174.jpg','images/bish1/PH-NemanjaZivkovic 175.jpg'],
+	"bish2": ['images/bish2/PH-NemanjaZivkovic 177.jpg','images/bish2/PH-NemanjaZivkovic 185.jpg','images/bish2/PH-NemanjaZivkovic 186.jpg','images/bish2/PH-NemanjaZivkovic 187.jpg','images/bish2/PH-NemanjaZivkovic 188.jpg'],
+	"s1": ['images/s1/PH-NemanjaZivkovic 179.jpg','images/s1/PH-NemanjaZivkovic 181.jpg','images/s1/PH-NemanjaZivkovic 182.jpg'],
+	"s2": ['images/s2/PH-NemanjaZivkovic 196.jpg'],
+	"s3": ['images/s3/PH-NemanjaZivkovic 198.jpg'],
+	"teenie": ['images/teenie/PH-NemanjaZivkovic 209.jpg','images/teenie/PH-NemanjaZivkovic 210.jpg','images/teenie/PH-NemanjaZivkovic 211.jpg'],
+};
+// var img1 = 'images/PH-NemanjaZivkovic 01.jpg';
+// var img2 = 'images/PH-NemanjaZivkovic 02.jpg';
+// var img3 = 'images/PH-NemanjaZivkovic 03.jpg';
+
+function showCarousel(num) {
+	$("#carouselContainer").show();
+	// for (var i = 0; i < images[num].size; i++) {
+	// 	$('#carouselContainer').append('<div class="item"><img src="'+images[num][i]+'" style="width:100%;"></div>');
+	// }
+	//console.log(images[num].length);
+	for (var i = 0; i < images[num].length; i++) {
+		if (i == 0) {
+			$('.carousel-indicators').append('<li data-target="#myCarousel" data-slide-to="'+i+'" class="active"></li>');
+			$('.carousel-inner').append('<div class="item active"><img src="'+images[num][i]+'" class="carouselImage"></img></div>');
+		} else {
+			$('.carousel-indicators').append('<li data-target="#myCarousel" data-slide-to="'+i+'"></li>');
+			$('.carousel-inner').append('<div class="item"><img src="'+images[num][i]+'" class="carouselImage"></img></div>');
+		}
+	}
+	//$('.carousel-inner').append('<div class="item active"><img src="'+images[num][1]+'" class="carouselImage"></img></div>');
+ 	$(".gallery-item").css("z-index", "-1");
+	$(".item>img").height($(window).height()-51);
+	$(".carouselClose").width(($(window).width()-$("#myCarousel").width())/2);
+}
+
+function hideCarousel() {
+	$("#carouselContainer").hide();
+	$('.carousel-indicators').empty();
+	$('.carousel-inner').empty();
+  	$(".gallery-item").css("z-index", "20");
+}
+
+// function showImage() {
+// 	$("#myCarousel").carousel("pause").removeData();
+// 	for (var i = 0; i < images.length; i++) {
+// 		// $('#carousel-container').append('<div class="item" id="carousel-item-'+i+'"></div>');
+// 		// $('#carousel-item-'+i).css('background-image', 'url("' + images[i] + '")')
+//     $('#carousel-container').append('<img class="item" src="'+images[i]+'" />');
+// 		if (i == 0) {
+// 			$('.item').addClass("active");
+// 		}
+// 	}
+// 	$('#myCarousel').show();
+// 	$('#myCarousel').carousel();
+// }
